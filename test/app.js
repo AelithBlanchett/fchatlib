a62aa0e52f16ba9dcecd8e9da5e5cecddd66c461
Aelith Blanchett <tonyteur@yahoo.fr>
2016-02-19 19:21:55 -0500

    corrected app.js

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
 var FChatLib = require('../lib/libfchat');
-var myFchatBot = new FChatLib(__dirname+'/config/config.js');
+var myFchatBot = new FChatLib(__dirname+'/config/config.example.js');
 myFchatBot.connect();
 console.log("ok");
