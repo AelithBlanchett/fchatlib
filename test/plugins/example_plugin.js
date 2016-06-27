6b7c54e44e3f057e5e3c974fc85681c5ec4b20eb
Aelith Blanchett <tonyteur@yahoo.fr>
2016-06-26 21:28:45 -0400

    fixed example commands

--- test/plugins/example_plugin.js
+++ test/plugins/example_plugin.js
@@ -1,3 +1,5 @@
+var uuid = require('uuid');
+
 module.exports = function (parent, args) {
     var fChatLibInstance = parent;
 
@@ -5,14 +7,19 @@ module.exports = function (parent, args) {
 
     cmdHandler.myString = "";
 
+    cmdHandler.randomId = uuid.v4();
 
     cmdHandler.hello = function (args, data) {
         var word = args || "everyone";
         fChatLibInstance.sendMessage(data.character +" wishes Bonjour! to "+word, data.channel);
     };
 
+    cmdHandler.random = function(args, data){
+        fChatLibInstance.sendMessage("Random seed: "+this.randomId.toString(), data.channel);
+    }
+
     function rollListener(parent, args){
-        fChatLibInstance.sendMessage("Wow! "+args.character+" just rolled a "+args.endresult+" !", data.channel);
+        fChatLibInstance.sendMessage("Wow! "+args.character+" just rolled a "+args.endresult+" !", args.channel);
     }
 
     fChatLibInstance.addRollListener(rollListener);
